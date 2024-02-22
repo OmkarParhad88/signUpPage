ec5cbe009762b9062d0710b6d3e7194c0563a659
Omkar <omkarparhad7905@gmail.com>
2024-02-22 14:23:34 +0530

    design a login and signup page

---
+++ src/component/Signin.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import validator from "validator";
+
+const Signin = () => {
+  const [message, setMessage] = useState(false);
+  const validateEmail = (e) => {
+    const email = e.target.value;
+
+    if (!validator.isEmail(email)) {
+      setMessage("Please, enter valid Email!");
+    }
+  };
+  return (
+    <div>
+      <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+        <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+          <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
+            Login
+          </h1>
+          <form className="mt-6">
+            <div className="mb-2">
+              <label
+                for="name"
+                className="block text-sm font-semibold text-gray-800"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div className="mb-2">
+              <label
+                for="email"
+                className="block text-sm font-semibold text-gray-800"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                onChange={(e) => validateEmail(e)}
+              />
+            </div>
+
+            <div className="mb-2">
+              <label
+                for="password"
+                className="block text-sm font-semibold text-gray-800"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+
+            <div class="flex items-center mt-7 mb-7">
+              <input
+                id="link-checkbox"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                for="link-checkbox"
+                class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                I agree with the{" "}
+                <a
+                  href="#"
+                  class="text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  terms and conditions
+                </a>
+                .
+              </label>
+            </div>
+
+            <div className="mt-6">
+              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                Login
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-8 text-xs font-light text-center text-gray-700">
+            {" "}
+            Do have an account?{" "}
+            <a href="#" className="font-medium text-purple-600 hover:underline">
+              Signup
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
