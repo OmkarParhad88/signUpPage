7a5f6d2121dfd51320121c74b3274bd19e9c244c
Omkar <omkarparhad7905@gmail.com>
2024-02-23 13:58:29 +0530

    the sign in signup was done and dcrypt password

--- src/component/Signin.jsx
+++ src/component/Signin.jsx
@@ -1,57 +1,46 @@
 import React, { useState } from "react";
-import validator from "validator";
 import { Link } from "react-router-dom";
+import validator from "validator";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Signin = () => {
-  const [message, setMessage] = useState("");
-  const [checkbox, setCheckbox] = useState();
-  const [name, setName] = useState();
+  const [message, setMessage] = useState(true);
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const navigate = useNavigate();
+
+  const submitData = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/signin", { email, password })
+      .then((result) => {
+        console.log(result);
+        if (result.data === "Success") {
+          navigate("/home");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
   const validateEmail = (e) => {
     const email = e.target.value;
-    setEmail(e.target.value);
-    if (email) setMessage("");
+    setEmail(email);
+    if (email === "") setMessage();
 
     if (validator.isEmail(email)) {
-      setMessage("true");
+      setMessage(true);
     } else {
       setMessage(false);
     }
   };
-  const handlecheckbox = () => {
-    setCheckbox(!checkbox);
-  };
-  const submitData = (e) => {
-    e.preventDefault();
-    axios
-      .post("http://localhost:3001/signin", { name, email, password })
-      .then((result) => console.log(result))
-      .catch((err) => console.log(err));
-  };
-
   return (
     <div>
       <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
         <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
           <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
-            Login
+            Sign in
           </h1>
           <form className="mt-6" onSubmit={submitData}>
-            <div className="mb-2">
-              <label
-                htmlFor="name"
-                className="block text-sm font-semibold text-gray-800"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
             <div className="mb-2">
               <label
                 htmlFor="email"
@@ -62,10 +51,9 @@ const Signin = () => {
               <input
                 type="email"
                 className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                onChange={(e) => validateEmail(e)}
+                onChange={validateEmail}
               />
             </div>
-
             <div className="mb-2">
               <label
                 htmlFor="password"
@@ -80,21 +68,9 @@ const Signin = () => {
               />
             </div>
 
-            <div className="flex items-center mt-7 mb-7">
-              <input
-                id="link-checkbox"
-                type="checkbox"
-                checked={checkbox}
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                onChange={handlecheckbox}
-              />
-              <label
-                htmlFor="link-checkbox"
-                className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                I agree with the terms and conditions .
-              </label>
-            </div>
+            <a href="#" className="text-xs text-purple-600 hover:underline">
+              Forget Password?
+            </a>
 
             <div className="mt-6">
               <button
@@ -105,9 +81,8 @@ const Signin = () => {
               </button>
             </div>
           </form>
-
           <p className="mt-8 text-xs font-light text-center text-gray-700">
-            Do have an account?
+            Don't have an account?
             <Link
               to="/signup"
               className="font-medium text-purple-600 hover:underline"
@@ -117,8 +92,7 @@ const Signin = () => {
           </p>
         </div>
       </div>
-
-      <span className=" items-center">{message}</span>
+      <p className=" items-center">{message}</p>
     </div>
   );
 };
--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -1,27 +1,63 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import validator from "validator";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  const [message, setMessage] = useState(true);
+  const [message, setMessage] = useState("");
+  const [checkbox, setCheckbox] = useState();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const navigate = useNavigate();
   const validateEmail = (e) => {
     const email = e.target.value;
-    if (email === "") setMessage();
+    setEmail(e.target.value);
+    if (email) setMessage("");
 
     if (validator.isEmail(email)) {
-      setMessage(true);
+      setMessage("true");
     } else {
       setMessage(false);
     }
   };
+  const handlecheckbox = () => {
+    setCheckbox(!checkbox);
+  };
+  const submitData = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/signup", { name, email, password })
+      .then((result) => {
+        console.log(result);
+        navigate("/home");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
       <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
         <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
           <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
-            Sign up
+            sign up
           </h1>
-          <form className="mt-6">
+          <form className="mt-6" onSubmit={submitData}>
+            <div className="mb-2">
+              <label
+                htmlFor="name"
+                className="block text-sm font-semibold text-gray-800"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
             <div className="mb-2">
               <label
                 htmlFor="email"
@@ -33,9 +69,9 @@ const Signup = () => {
                 type="email"
                 className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                 onChange={(e) => validateEmail(e)}
-                style={message ? {} : { border: "1px solid red" }}
               />
             </div>
+
             <div className="mb-2">
               <label
                 htmlFor="password"
@@ -46,30 +82,49 @@ const Signup = () => {
               <input
                 type="password"
                 className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
-            <a href="#" className="text-xs text-purple-600 hover:underline">
-              Forget Password?
-            </a>
+            <div className="flex items-center mt-7 mb-7">
+              <input
+                id="link-checkbox"
+                type="checkbox"
+                checked={checkbox}
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                onChange={handlecheckbox}
+              />
+              <label
+                htmlFor="link-checkbox"
+                className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                I agree with the terms and conditions .
+              </label>
+            </div>
+
+            <div className="mt-6">
+              <button
+                type="submit"
+                className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+              >
+                Login
+              </button>
+            </div>
           </form>
-          <div className="mt-6">
-            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-              Login
-            </button>
-          </div>
 
           <p className="mt-8 text-xs font-light text-center text-gray-700">
-            Don't have an account?
+            Do have an account?
             <Link
               to="/signin"
               className="font-medium text-purple-600 hover:underline"
             >
-              Sign in
+              Signin
             </Link>
           </p>
         </div>
       </div>
+
+      <p className=" items-center">{message}</p>
     </div>
   );
 };
